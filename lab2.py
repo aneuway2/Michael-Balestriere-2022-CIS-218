1d73196563ebf75ae5677cc6f044cc41c8554ed2
Michael <mbalestriere@mail.immaculata.edu>
2022-02-03 23:39:11 -0500

    resubmission

--- lab2.py
+++ lab2.py
@@ -4,9 +4,12 @@ Week 2 - Eggs
 URL - https://www.saudereggs.com/blog/egg-sizing-guide/
 '''
 
-while (True):
+while True:
     weight=float(input("How much does the egg weigh in ounces? between 0.0 and 2.5 ounces."))
-    if weight<=1.5:
+    
+    if weight <=0:
+        print("This is an invalid number!")
+    elif weight<=1.5:
         print("This is a size small egg!")
     elif weight<=1.75:
         print("This is a size medium egg!")
